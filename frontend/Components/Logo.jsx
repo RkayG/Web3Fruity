6a8057d612e3bb44ac0cf4c0b2600a12c6b4b84d
Your Name <you@example.com>
2024-07-19 23:02:31 +0100

    made ui changes

--- frontend/Components/Logo.jsx
+++ frontend/Components/Logo.jsx
@@ -14,3 +14,5 @@ export default Logo;
 {/* <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-orange-500 text-transparent bg-clip-text">
                 Web<span className="font-serif">3</span>Fruity
               </span> */}
+
+              
